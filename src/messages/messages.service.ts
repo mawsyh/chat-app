3e8525fd39682b1bd17866c99539b374516addaa
unknown <moh.mir36@gmail.com>
2023-03-04 00:43:13 +0330

    firstcommit

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  messages: Message[] = [{ name: 'Sirus', text: 'Yoooohooo' }];
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message = {
+      name: this.clientToUser[clientId],
+      text: createMessageDto.text,
+    };
+    this.messages.push(message);
+    return message;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} message`;
+  }
+
+  update(id: number, updateMessageDto: UpdateMessageDto) {
+    return `This action updates a #${id} message`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} message`;
+  }
+}
